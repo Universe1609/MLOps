f84ec34da450aff2b99c16c8091de93a0a14bd34
Universe <davi_9_d@hotmail.com>
2023-07-27 01:00:42 -0500

    Explicacion
    Se actualiza el codigo para una mejor explicacion del codigo hecho.
    
    Este script utiliza un modelo preentrenado para predecir datos de un censo de adultos basándose en la edad, la ganancia de capital, la pérdida de capital y las horas de trabajo por semana

--- despliegue_ml/deploy.py
+++ despliegue_ml/deploy.py
@@ -1,48 +1,51 @@
+# Importamos el módulo pickle para serializar y deserializar objetos de Python.
 import pickle
 
+# Importamos click para crear interfaces de línea de comandos en Python.
 import click
+# Importamos numpy para realizar operaciones numéricas y manipular matrices.
 import numpy as np
+# Importamos pandas para manipular y analizar datos.
 import pandas as pd
 
+# Configuramos numpy para ignorar todos los errores.
 np.seterr(all="ignore")
 
-
+# Definimos un nuevo comando de línea de comandos con click.
 @click.command()
+# Añadimos una opción al comando para introducir los datos de entrada.
 @click.option(
     "--input_data",
     "-i",
-    help="Values of age, capital-gain, capital-loss and hours-per-week separated by comma",
+    help="Valores de edad, ganancia de capital, pérdida de capital y horas por semana separados por comas",
 )
 def predict_adult_census(input_data: str) -> np.ndarray:
     """
-    Runs a predition on the adult census data using a serialized logistic regression model.
-
-    :param input_data: A string containing values for agem capital-gain, capital-loss, and hours-per-week
-        separated by commas.
-    :type input_data: str
-    :return: An array of predicted values.
-    :rtype: numpy.ndarray
+    Función principal que usa un modelo de regresión logística previamente entrenado para hacer predicciones en base a los datos del censo de adultos.
     """
 
-    # cargando el modelo serializado
+    # Cargamos el modelo serializado desde un archivo.
     with open("../models/adult_census_lr.pkl", "rb") as f:
         trained_model = pickle.load(f)
 
-    # Preparando la data como un DataFrame
+    # Creamos un DataFrame de pandas a partir de los datos de entrada.
     input_df = pd.DataFrame(
         [input_data.split(",")],
         columns=["age", "capital-gain", "capital-loss", "hours-per-week"],
     )
+    # Cambiamos el tipo de datos del DataFrame a int.
     input_df = input_df.astype(int)
 
-    # HACIOENDO PREDICCIONES
+    # Utilizamos el modelo para hacer predicciones en base a los datos de entrada.
     predictions = trained_model.predict(input_df)
 
+    # Imprimimos cada predicción en la consola.
     for prediction in predictions:
         click.echo(prediction)
 
+    # Devolvemos las predicciones como resultado de la función.
     return predictions
 
-
+# Si el script se está ejecutando como programa principal, llamamos a la función predict_adult_census.
 if __name__ == "__main__":
     predict_adult_census()
